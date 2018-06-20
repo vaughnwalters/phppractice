9d5e6e79e43b3b2471d223e4b17938661ade218b
Vaughn Walters <vwalters@paramoredigital.com>
2018-06-20 11:24:23 -0500

    refactoring form to send to external page to process

---
+++ scratchpad/form_external.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="form_process.php" method="post">
+
+    <input type="text" name="username" placeholder="Enter Name">
+    <input type="password" name="password" placeholder="Enter Password"><br>
+    <input type="submit" name="submit">
+
+</form>
+
+</body>
+</html>
