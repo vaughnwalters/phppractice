29f845e17bf9242ce4f2417fa6829b6a8133cce6
Vaughn Walters <vwalters@paramoredigital.com>
2018-06-20 13:01:34 -0500

    creating for that will submit to newly created mysql table

---
+++ scratchpad/mysql/login.php
@@ -0,0 +1,37 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="col-xs-6">
+        <form action="login" method="post">
+
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+
+            <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+
+        </form>
+    </div>
+</div>
+
+
+</body>
+</html>
